385a54b214e8e171340bbfc2a1b5c54dd95accb9
AapoRajala <vrajala24@students.oamk.fi>
2026-01-14 15:41:38 +0200

    Viikko Tehtävät 1
    Tehtävä tehty

---
+++ app/src/main/java/com/example/viikko1/domain/MockData.kt
@@ -0,0 +1,14 @@
+package com.example.viikko1.domain
+
+val mockTasks = listOf(
+    Task( id = 1, title = "Market", description = "Maitoa ja leipää", priority = 1, dueDate ="2026-1-18", done = false),
+            Task( id = 2, title = "Car fix", description = "Öljyt", priority = 1, dueDate ="2026-1-14", done = true),
+                    Task(id = 3, title = "Lift day", description = "Haukka / selkä", priority = 1, dueDate ="2026-1-12", done = false),
+                            Task(id = 4, title = "Leg day", description = "Skip", priority = 1, dueDate ="2026-1-13", done = false),
+                                    Task(id = 5, title = "Push day", description = "Rinta / ojentaja", priority = 1, dueDate ="2026-1-13", done = true)
+
+
+
+
+
+)
